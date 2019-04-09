061bcc73a15a4bdf6b18f33e34ab52bf09c61c45
jverea <jvereagovan2018@gmail.com>
2019-04-09 12:35:14 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,25 @@
+public class TableAndChairs {
+    //Author: jverea
+    //Date: 04/09/2019
+    //Project: Tables and Chairs
+
+    public static void main (String[] args){
+
+        System.out.println("Building Some Tables And Chairs");
+        System.out.println("/n/n/n/n");
+
+        System.out.println("C");
+        System.out.println("H");
+        System.out.println("A");
+        System.out.println("I          TABLETABLETABLETABLE");
+        System.out.println("R            L       ");
+        System.out.println("CHAIRCHAIR   E");
+        System.out.println("C        C   G");
+        System.out.println("H        H   L");
+        System.out.println("A        A   E");
+        System.out.println("I        I   G");
+        System.out.println("R        R   !");
+
+
+    }
+}
