372c091701dee02291b95695951cbcfcf53e975d
jverea <jvereagovan2018@gmail.com>
2019-04-10 12:53:48 -0500

    Update

---
+++ src/MovieQuoteInfo.java
@@ -0,0 +1,9 @@
+public class MovieQuoteInfo {
+    //Author: jverea
+    //Date: 04/10/2019
+    //Project: MovieQuoteInfo
+    public static void main(String[]args){
+
+        System.out.println("MayTheForceBeWithYou");
+    }
+}
---
+++ src/TacoTruck.java
@@ -0,0 +1,33 @@
+public class TacoTruck {
+
+    public static void main (String[] args){
+
+        System.out.println("_________________________________________");
+        System.out.println("--------Welcome To Delgados Tacos--------");
+        System.out.println("_________________________________________");
+
+
+
+
+    }
+    public static void displayTacoTruck(){
+
+        System.out.println("_________________________________________");
+        System.out.println("|                                       |");
+        System.out.println("|                                       |");
+        System.out.println("|                                       |");
+        System.out.println("|                                       |");
+        System.out.println("|                                       |");
+        System.out.println("_________________________________________________________");
+        System.out.println("|                                       |    {          |");
+        System.out.println("|                                       |    {          |");
+        System.out.println("|                                       |    {__________|");
+        System.out.println("|                                       |               |");
+        System.out.println("|                                       |               |");
+        System.out.println("|                                       |               |");
+        System.out.println("________________________________________|________________");
+    }
+}
+
+
+
